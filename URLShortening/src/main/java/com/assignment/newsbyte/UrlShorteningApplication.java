e3b69aaba7f51cc11d1118a29205d7a04a485e15
erabhhishekgupta <abhi7510037174@gmail.com>
2024-10-02 18:48:34 +0530

    debugging done to check which application properties is working

--- URLShortening/src/main/java/com/assignment/newsbyte/UrlShorteningApplication.java
+++ URLShortening/src/main/java/com/assignment/newsbyte/UrlShorteningApplication.java
@@ -1,13 +1,28 @@
 package com.assignment.newsbyte;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.env.Environment;
+
 @SpringBootApplication
 public class UrlShorteningApplication {
-
+	  @Autowired
+	  private Environment env;
 	public static void main(String[] args) {
+		
+		 
 		SpringApplication.run(UrlShorteningApplication.class, args);
 	}
+	 //to get the active log profile
+	 @EventListener(ApplicationReadyEvent.class)
+	    public void logActiveProfiles() {
+	        String[] activeProfiles = env.getActiveProfiles();
+	        System.out.println("Active Profiles: " + String.join(", ", activeProfiles));
+	    }
 
 }
